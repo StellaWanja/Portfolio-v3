e886ffcef29bf8bd4336da98a01c2238334c62a1
StellaWanja <stylades@gmail.com>
2024-03-28 12:51:08 +0300

    Added animation effects

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,10 @@
-import { FaLinkedinIn, FaGithub, FaDribbble, FaBehance, FaTwitter  } from "react-icons/fa";
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaDribbble,
+  FaBehance,
+  FaTwitter,
+} from "react-icons/fa";
 import "./Contact.css";
 import { Link } from "react-router-dom";
 
@@ -11,26 +17,38 @@ const Contact = () => {
         have a question or just want to connect.
       </p>
       <div className="social-media">
-      <Link to="https://github.com/StellaWanja" target="_blank">
-          <FaGithub />
+        <Link to="https://github.com/StellaWanja" className="icon" target="_blank">
+          <FaGithub  />
         </Link>
         <Link
           to="https://www.linkedin.com/in/stella-w-njoroge/"
+          className="icon"
           target="_blank"
         >
           <FaLinkedinIn />
         </Link>
-        <Link to="https://twitter.com/StylaDes" target="_blank">
+        <Link
+          to="https://twitter.com/StylaDes"
+          className="icon"
+          target="_blank"
+        >
           <FaTwitter />
         </Link>
-        <Link to="https://dribbble.com/StylaDes" target="_blank">
+        <Link
+          to="https://dribbble.com/StylaDes"
+          className="icon"
+          target="_blank"
+        >
           <FaDribbble />
         </Link>
-        <Link to="https://www.behance.net/stylades" target="_blank">
+        <Link
+          to="https://www.behance.net/stylades"
+          className="icon"
+          target="_blank"
+        >
           <FaBehance />
         </Link>
       </div>
-      
     </section>
   );
 };
